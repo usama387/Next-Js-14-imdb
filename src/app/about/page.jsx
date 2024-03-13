1719d494fc4451017ecd36ce5346d8fc554adca8
usama387 <128306049+usama387@users.noreply.github.com>
2024-03-13 08:39:41 +0500

    About Page Completed

---
+++ src/app/about/page.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const AboutPage = () => {
+  return (
+    <div className="max-w-6xl mx-auto p-3 space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Discover the mesmerizing world of cinema with our movie site! From
+        heart-pounding thrillers to heartwarming dramas, we have got it all. Dive
+        into a sea of captivating stories, where every click unveils a new
+        adventure.
+      </p>
+
+      <p>
+        Discover the mesmerizing world of cinema with our movie site! From
+        heart-pounding thrillers to heartwarming dramas, we have got it all. Dive
+        into a sea of captivating stories, where every click unveils a new
+        adventure.
+      </p>
+      <p>
+        Welcome to the ultimate destination for movie enthusiasts! Explore a
+        vast collection of films ranging from timeless classics to the latest
+        releases. Whether you seeking laughter, tears, or spine-tingling
+        suspense, our movie site promises an unforgettable experience at every
+        click.
+      </p>
+    </div>
+  );
+};
+
+export default AboutPage;
