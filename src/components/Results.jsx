bb1342570d7d259bd43fe82eec58072c04a16d58
usama387 <128306049+usama387@users.noreply.github.com>
2024-03-14 13:32:33 +0500

    Added Card component and connected with api to show results

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,13 +1,13 @@
 import React from "react";
+import Card from "./Card";
 
 const Results = ({ results }) => {
   return (
-    // #1 Here i map the results coming as prop from tmdb api
-    <div>
+    // #1 Adding responsiveness to card grid on different screens
+    <div className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 max-w-6xl mx-auto py-4">
+      {/* #2 Here i map the results coming as prop from page.jsx and then i send result as prop to my card component */}
       {results.map((result) => (
-        <div key={result.id}>
-          <h2>{result.original_title}</h2>
-        </div>
+        <Card key={result.id} result={result} />
       ))}
     </div>
   );
